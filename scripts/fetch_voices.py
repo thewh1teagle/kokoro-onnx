a28a14441e4360a09a02615425d92769ffb04134
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-01-29 23:48:17 +0200

    fix: dynamically fetch voice names from Hugging Face repository

--- scripts/fetch_voices.py
+++ scripts/fetch_voices.py
@@ -16,27 +16,21 @@ import io
 import numpy as np
 import requests
 import torch
-
-names = [
-    "af",
-    "af_bella",
-    "af_nicole",
-    "af_sarah",
-    "af_sky",
-    "am_adam",
-    "am_michael",
-    "bf_emma",
-    "bf_isabella",
-    "bm_george",
-    "bm_lewis",
-]
+import re
 
 pattern = "https://huggingface.co/hexgrad/Kokoro-82M/resolve/main/voices/{name}.pt"
+url = "https://huggingface.co/hexgrad/Kokoro-82M/tree/main/voices"
 voices = {}
 
-for name in names:
+names = re.findall(
+    'href="/hexgrad/Kokoro-82M/blob/main/voices/(.+).pt', requests.get(url).text
+)
+print(", ".join(names))
+
+count = len(names)
+for i, name in enumerate(names, 1):
     url = pattern.format(name=name)
-    print(f"Downloading {url}")
+    print(f"Downloading {url} ({i}/{count})")
     r = requests.get(url)
     r.raise_for_status()  # Ensure the request was successful
     content = io.BytesIO(r.content)
