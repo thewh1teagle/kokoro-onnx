ad08449ae55b82d7b89bd7c597dbdd0999ba5e47
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-03-01 20:57:50 +0200

    add more languages examples

---
+++ examples/japanse.py
@@ -0,0 +1,32 @@
+"""
+Usage:
+1.
+    Install uv from https://docs.astral.sh/uv/getting-started/installation
+2.
+    Copy this file to new folder
+3.
+    Run
+    uv venv -p 3.12
+    uv pip install -U kokoro-onnx==0.4.4 soundfile==0.13.1 'misaki[ja]==0.8.4'
+3.
+    Download these files
+    https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.1/kokoro-v1.1-zh.onnx
+    https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.1/voices-v1.1-zh.bin
+4. Run
+    uv run main.py
+"""
+
+import soundfile as sf
+from kokoro_onnx import Kokoro
+from misaki import ja
+
+# Misaki G2P with espeak-ng fallback
+g2p = ja.JAG2P()
+
+text = "「人生を夢見るな。夢を生きろ。」"
+voice = "jf_alpha"
+kokoro = Kokoro("kokoro-v1.0.onnx", "voices-v1.0.bin")
+phonemes, _ = g2p(text)
+samples, sample_rate = kokoro.create(phonemes, voice=voice, speed=1.0, is_phonemes=True)
+sf.write("audio.wav", samples, sample_rate)
+print("Created audio.wav")
---
+++ examples/portuguese.py
@@ -0,0 +1,41 @@
+"""
+Usage:
+1.
+    Install uv from https://docs.astral.sh/uv/getting-started/installation
+2.
+    Copy this file to new folder
+3.
+    Download these files
+    https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/kokoro-v1.0.onnx
+    https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin
+4. Run
+    uv venv --seed -p 3.12
+    source .venv/bin/activate
+    uv pip install -U kokoro-onnx==0.4.4 soundfile==0.13.1 "misaki[en]==0.8.4"
+    uv run main.py
+
+For other languages read https://huggingface.co/hexgrad/Kokoro-82M/blob/main/VOICES.md
+"""
+
+import soundfile as sf
+from kokoro_onnx import Kokoro
+from misaki import en, espeak
+from misaki.espeak import EspeakG2P
+
+# Misaki G2P with espeak-ng fallback
+fallback = espeak.EspeakFallback(british=False)
+g2p = EspeakG2P(language='pt-br')
+
+# Kokoro
+kokoro = Kokoro("kokoro-v1.0.onnx", "voices-v1.0.bin")
+
+# Phonemize
+text = "Não sonhe sua vida, viva seu sonho."
+phonemes, _ = g2p(text)
+
+# Create
+samples, sample_rate = kokoro.create(phonemes, "pf_dora", is_phonemes=True)
+
+# Save
+sf.write("audio.wav", samples, sample_rate)
+print("Created audio.wav")
